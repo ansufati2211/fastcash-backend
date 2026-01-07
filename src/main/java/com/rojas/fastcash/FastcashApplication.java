bf7e6f641e230b930bf3baeaa1009f77c768066d
Gabriel Yllescas <gabaledan@gmail.com>
2026-01-07 12:22:54 -0500

    Punto 1

---
+++ src/main/java/com/rojas/fastcash/FastcashApplication.java
@@ -0,0 +1,13 @@
+package com.rojas.fastcash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastcashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rojas/fastcash/FastcashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rojas.fastcash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
